d4d819c0a11733d47efd061fccc63db955550dff
hemanthallugunti <93366548+alluguntihemanth@users.noreply.github.com>
2024-10-23 19:28:21 +0530

    Update google_fit_api.py

--- google_fit_api.py
+++ google_fit_api.py
@@ -29,31 +29,35 @@ def authenticate_user():
 
     return None
 
+def fetch_heart_rate_data(creds):
+    """Fetches heart rate data from Google Fit API using user credentials."""
+    service = build('fitness', 'v1', credentials=creds)
 
+    # Fetch heart rate data
+    data_sources = service.users().dataSources().list(userId='me').execute()
+    heart_rate_source = None
 
-def authenticate_user():
-    flow = Flow.from_client_secrets_file(
-        'credentials/client_secrets.json',
-        scopes=['https://www.googleapis.com/auth/fitness.heart_rate.read'],
-        redirect_uri='https://project-matthew-hemanthallugunti.streamlit.app/'  # Use your app's deployed URL
-    )
+    for data_source in data_sources['dataSource']:
+        if 'com.google.heart_rate.bpm' in data_source['dataStreamId']:
+            heart_rate_source = data_source
+            break
 
-    if 'credentials' in st.session_state:
-        return st.session_state['credentials']
+    if heart_rate_source:
+        dataset = service.users().dataSources().datasets().get(
+            userId='me',
+            dataSourceId=heart_rate_source['dataStreamId'],
+            datasetId="0-{}".format(int(time.time() * 1000000000))
+        ).execute()
 
-    # Check for authorization code in query params
-    query_params = st.query_params()
-    
-    if 'code' in query_params:
-        code = query_params['code'][0]  # Make sure to extract the code correctly
-        flow.fetch_token(authorization_response=f"https://project-matthew-hemanthallugunti.streamlit.app/?code={code}")
-        creds = flow.credentials
-        st.session_state['credentials'] = creds
-        return creds
-    else:
-        auth_url, _ = flow.authorization_url(access_type='offline', include_granted_scopes='true')
-        st.write(f'Please authorize the application: [Authorize Here]({auth_url})')
-
+        if 'point' in dataset:
+            heart_rate_data = []
+            for point in dataset['point']:
+                heart_rate_data.append({
+                    'start_time': point['startTimeNanos'],
+                    'end_time': point['endTimeNanos'],
+                    'heart_rate': point['value'][0]['fpVal']
+                })
+            return heart_rate_data
     return None
 
 
